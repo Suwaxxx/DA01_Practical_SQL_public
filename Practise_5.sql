7a1a07aae68d25d1e7d9050a47db45e8543f9eb7
Suwaxxx <126514275+Suwaxxx@users.noreply.github.com>
2024-07-24 00:54:35 +0700

    Update Practise_5.sql

--- Practise_5.sql
+++ Practise_5.sql
@@ -7,4 +7,9 @@ select count(city)-count(distinct city) from station
 select ceiling(avg(salary)-avg(salary?)) from employees
 --Baitap4 
 select round(sum(item_count::DECIMAL*order_occurrences)/sum(order_occurrences),1) as avg_items FROM items_per_order
-
+--Baitap5
+SELECT candidate_id FROM candidates
+where skill in ('Python', 'Tableau', 'PostgreSQL')  
+group by candidate_id
+having count(skill)=3
+order by candidate_id 
