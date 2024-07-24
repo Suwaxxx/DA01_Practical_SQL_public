458972754a48cfbcc041840f4995ddcccb2f41d0
Suwaxxx <126514275+Suwaxxx@users.noreply.github.com>
2024-07-24 18:24:36 +0700

    Update Practise_5.sql

--- Practise_5.sql
+++ Practise_5.sql
@@ -3,7 +3,7 @@ select distinct city from station
 where id%2=0
 --Baitap2
 select count(city)-count(distinct city) from station
---Baitap3 em khom biet lammm
+--Baitap3 em khom biet lammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
 select ceiling(avg(salary)-avg(salary?)) from employees
 --Baitap4 
 select round(sum(item_count::DECIMAL*order_occurrences)/sum(order_occurrences),1) as avg_items FROM items_per_order
@@ -13,3 +13,40 @@ where skill in ('Python', 'Tableau', 'PostgreSQL')
 group by candidate_id
 having count(skill)=3
 order by candidate_id 
+--Baitap6
+SELECT user_id,
+max(DATE(post_date)) - min(DATE(post_date)) as count_date
+FROM posts
+where DATE(post_date)<='12/31/2021' and DATE(post_date) >= '01/01/2021'
+group by user_id
+having count(user_id)>=2
+--Baitap7
+select card_name , 
+max(issued_amount)-min(issued_amount) as disparity
+from monthly_cards_issued
+group by card_name
+order by disparity desc
+--Baitap8
+SELECT manufacturer, count(product_id), sum(cogs-total_sales) as total_losses
+FROM pharmacy_sales
+where  total_sales<cogs
+group by manufacturer
+order by total_losses desc
+--Baitap9
+select * from cinema
+where id%2=1 and not description like '%boring%'
+order by rating desc
+--Baitap10
+select teacher_id, count(distinct subject_id) as cnt
+from teacher
+group by teacher_id
+--Baitap11
+select user_id, count(follower_id) as followers_count
+from followers
+group by user_id
+order by user_id
+--Baitap12
+select class
+from courses
+group by class
+having  count(student)>=5
